317d179d93fcd4a4ef3a77952cc9db69c153e34a
takeda <yoichiro_takeda@forcia.com>
2022-11-01 22:40:00 +0900

    Add authentication

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,13 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "cognitousermanager7a2ef68e": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -1,9 +1,30 @@
-import "../styles/globals.css";
-import "antd/dist/antd.dark.css";
+import { Authenticator } from "@aws-amplify/ui-react";
+import { Amplify } from "aws-amplify";
 import { AppPropsWithLayout } from "../types";
 import { MainLayout } from "../layouts/MainLayout";
+import awsExports from "../src/aws-exports";
+
+import "../styles/globals.css";
+import "antd/dist/antd.dark.css";
+import "@aws-amplify/ui-react/styles.css";
+
+Amplify.configure({ ...awsExports, ssr: true });
+
+const components = {
+  Header() {
+    return <div style={{ padding: "24px 0" }}>loging header sample</div>;
+  },
+};
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? MainLayout;
-  return <>{getLayout(<Component {...pageProps} />)}</>;
+  return (
+    <Authenticator.Provider>
+      {getLayout(
+        <Authenticator hideSignUp components={components}>
+          <Component {...pageProps} />
+        </Authenticator>,
+      )}
+    </Authenticator.Provider>
+  );
 }
